6f74ba9f071cbe6d429fabf69100b4c9746980f4
Ari <127914863+isaAJ05@users.noreply.github.com>
2025-04-25 21:19:14 -0500

    prueba integracion base de datos

---
+++ react-admin-dashboard-master/sqltest.js
@@ -0,0 +1,73 @@
+const sql = require('mssql');
+
+const config = {
+    user: 'adminsql', // better stored in an app setting such as process.env.DB_USER
+    password: 'NIPS-lab#1', // better stored in an app setting such as process.env.DB_PASSWORD
+    server: 'servidorskysql.database.windows.net', // better stored in an app setting such as process.env.DB_SERVER
+    port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
+    database: 'FlightsData', // better stored in an app setting such as process.env.DB_NAME
+    authentication: {
+        type: 'default'
+    },
+    options: {
+        encrypt: true
+    }
+}
+
+/*
+    //Use Azure VM Managed Identity to connect to the SQL database
+    const config = {
+        server: process.env["db_server"],
+        port: process.env["db_port"],
+        database: process.env["db_database"],
+        authentication: {
+            type: 'azure-active-directory-msi-vm'
+        },
+        options: {
+            encrypt: true
+        }
+    }
+
+    //Use Azure App Service Managed Identity to connect to the SQL database
+    const config = {
+        server: process.env["db_server"],
+        port: process.env["db_port"],
+        database: process.env["db_database"],
+        authentication: {
+            type: 'azure-active-directory-msi-app-service'
+        },
+        options: {
+            encrypt: true
+        }
+    }
+*/
+
+console.log("Starting...");
+connectAndQuery();
+
+async function connectAndQuery() {
+    try {
+        var poolConnection = await sql.connect(config);
+
+        console.log("Reading rows from the Table...");
+        var resultSet = await poolConnection.request().query(`
+          SELECT COUNT(*) AS Total_Filas
+        FROM Flights`);
+
+        console.log(`${resultSet.recordset.length} rows returned.`);
+
+        // output column headers
+        var columns = Object.keys(resultSet.recordset[0]).join(", ");
+        console.log("%s", columns);
+
+        // output row contents from default record set
+        resultSet.recordset.forEach(row => {
+            console.log("%s", row.Total_Filas);
+        });
+
+        // close connection only when we're certain application is finished
+        poolConnection.close();
+    } catch (err) {
+        console.error(err.message);
+    }
+}
