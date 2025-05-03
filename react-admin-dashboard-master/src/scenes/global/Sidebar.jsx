de15b566cb8d7b65fdcac7c773e5ea04c85fa428
Sebastian Brito <141294050+sbritoa20@users.noreply.github.com>
2025-05-03 17:01:19 -0500

    quitar form

--- react-admin-dashboard-master/src/scenes/global/Sidebar.jsx
+++ react-admin-dashboard-master/src/scenes/global/Sidebar.jsx
@@ -142,15 +142,7 @@ const Sidebar = () => {
             >
               Pages
             </Typography>
-            <Item
-              title="Profile Form"
-              to="/form"
-              icon={<PersonOutlinedIcon />}
-              selected={selected}
-              setSelected={setSelected}
-            />
-
-
+        
             <Item
               title="Proposito"
               to="/faq"
