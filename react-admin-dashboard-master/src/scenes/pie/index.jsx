aad822abd3bd621ceecb1bea8c0979f5f147bbb4
Sebastian Brito <141294050+sbritoa20@users.noreply.github.com>
2025-05-02 19:04:49 -0500

    ahorasi

--- react-admin-dashboard-master/src/scenes/pie/index.jsx
+++ react-admin-dashboard-master/src/scenes/pie/index.jsx
@@ -1,6 +1,7 @@
 import { Box, IconButton } from "@mui/material";
 import Header from "../../components/Header";
 import PieChart from "../../components/PieChart";
+import RefreshOutlinedIcon from '@mui/icons-material/RefreshOutlined';
 
 
 const Pie = () => {
