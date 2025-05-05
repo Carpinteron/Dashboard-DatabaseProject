2bdf4912cf2598a381de4b206cc9e376b0931c76
Sebastian Brito <141294050+sbritoa20@users.noreply.github.com>
2025-05-04 19:55:44 -0500

    pie

--- react-admin-dashboard-master/src/scenes/pie/index.jsx
+++ react-admin-dashboard-master/src/scenes/pie/index.jsx
@@ -8,9 +8,15 @@ import {
 import { useState } from "react";
 import Header from "../../components/Header";
 import PieChart from "../../components/PieChart";
+import { useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+
 
 const Pie = () => {
   const [opcion, setOpcion] = useState("visitada");
+  const theme = useTheme();
+   const colors = tokens(theme.palette.mode);
+
 
   // Mapeo de títulos según la opción seleccionada
   const titulos = {
@@ -21,8 +27,9 @@ const Pie = () => {
   return (
     <Box m="20px">
       <Header title={titulos[opcion]} subtitle="Pie Chart" />
-
-      <FormControl sx={{ minWidth: 250, mb: 2 }}>
+      
+      <FormControl sx={{ minWidth: 210, mb: 2 }}>
+      <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="1px">
         <InputLabel>Selecciona top</InputLabel>
         <Select
           value={opcion}
@@ -32,8 +39,9 @@ const Pie = () => {
           <MenuItem value="visitada">Top 5 Ciudades más visitadas</MenuItem>
           <MenuItem value="salidas">Top 5 Ciudades con más vuelos de salida</MenuItem>
         </Select>
+        </Box>
       </FormControl>
-
+      
       <Box height="75vh">
         <PieChart tipo={opcion} />
       </Box>
