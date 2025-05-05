b4357368f682b6f6ef5b0fc24b8fcfdc79b12cc6
pau <127871414+pzarante@users.noreply.github.com>
2025-05-04 20:51:13 -0500

    mini cambio

--- react-admin-dashboard-master/Queries/queries_franja.sql
+++ react-admin-dashboard-master/Queries/queries_franja.sql
@@ -7,8 +7,15 @@ select avg(cast(f.nsmiles as float)) as avgdistancia
 from Flights_US f
 
 -- total aeropuertos
-select count(distinct f.airport1) + count(distinct f.airport2) as totalaeropuertos
-from Flights_US f
+with Aeropuertos as (
+    select f.airport1 as aeropuerto
+	from Flights_US f
+    union 
+    select f.airport2 
+	from Flights_US f
+)
+select count(*) totalaeropuertos
+from Aeropuertos;
 
 -- precio promedio
 select avg(cast(f.fare as float)) as avgprecio
